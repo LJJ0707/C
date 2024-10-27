e62f2f35c678c735af0636e86c88c63b29c908d6
ljj <2252468311@qq.com>
2024-10-27 18:50:41 +0800

    对循坏和求和，自增自减运算有更深的理解的一些注释

--- 经典示例/008.数列求和/8.C
+++ 经典示例/008.数列求和/8.C
@@ -17,7 +17,9 @@ main()
 		temp=0;
 		for(j=1;j<=i;j++)
 			temp+=j;  //temp=temp+j ,计算从1加到i的自然数累加和
+		           //更加形象的理解就是：可以将temp看成是一个小盒子，将内循环j进行累加，然后放在这个小盒子里
 		sum+=temp;  //sum=sum+temp,计算每个的i累加和加到总和中
+		//可以将sum看成一个大盒子，将小盒子再放到大盒子中
 	}
 	printf("The sum of the sequence(%d) is %d\n",n,sum);
 	getchar();
